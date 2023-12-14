9637b29e446641e8cb286d9e0c2d50cd1e55d0d6
Cerberose <tranlethaison123@gmail.com>
2023-12-14 11:23:25 +0700

    first push

---
+++ app/src/main/java/com/example/mangaapp_finalproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mangaapp_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
